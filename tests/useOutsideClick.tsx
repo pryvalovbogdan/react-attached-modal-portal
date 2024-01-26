ad8cf06d233e85a61d15a631a85e21589facc33a
bogdanpryvalov <pryvalov.bogddan@gmail.com>
2024-01-26 18:22:20 +0200

    added useOutsideClick

---
+++ tests/useOutsideClick.tsx
@@ -0,0 +1,71 @@
+import { fireEvent } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useOutsideClick } from '../src/hooks/useOutsideClick';
+
+describe('useOutsideClick hook', () => {
+  it('should call the callback when a click outside the ignored refs occurs', () => {
+    const callback = jest.fn();
+    const ref1 = { current: document.createElement('div') };
+    const ref2 = { current: document.createElement('div') };
+    const ignoredRefs = [ref1, ref2];
+
+    renderHook(() => useOutsideClick(ignoredRefs, callback, true));
+
+    fireEvent.click(document);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call the callback when a click inside any of the ignored refs occurs', () => {
+    const callback = jest.fn();
+    const ref1 = { current: document.createElement('div') };
+    const ref2 = { current: document.createElement('div') };
+    const ignoredRefs = [ref1, ref2];
+
+    renderHook(() => useOutsideClick(ignoredRefs, callback, true));
+
+    fireEvent.click(ref1.current);
+
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('should call the callback when a touch event outside the ignored refs occurs', () => {
+    const callback = jest.fn();
+    const ref1 = { current: document.createElement('div') };
+    const ref2 = { current: document.createElement('div') };
+    const ignoredRefs = [ref1, ref2];
+
+    renderHook(() => useOutsideClick(ignoredRefs, callback, true));
+
+    fireEvent.touchStart(document);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call the callback when a touch event inside any of the ignored refs occurs', () => {
+    const callback = jest.fn();
+    const ref1 = { current: document.createElement('div') };
+    const ref2 = { current: document.createElement('div') };
+    const ignoredRefs = [ref1, ref2];
+
+    renderHook(() => useOutsideClick(ignoredRefs, callback, true));
+
+    fireEvent.touchStart(ref1.current);
+
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('should not call the callback when isOpenPanel is false', () => {
+    const callback = jest.fn();
+    const ref1 = { current: document.createElement('div') };
+    const ref2 = { current: document.createElement('div') };
+    const ignoredRefs = [ref1, ref2];
+
+    renderHook(() => useOutsideClick(ignoredRefs, callback, false));
+
+    fireEvent.click(document);
+
+    expect(callback).not.toHaveBeenCalled();
+  });
+});
